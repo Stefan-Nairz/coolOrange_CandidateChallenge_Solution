54cb4ab533a35e948645b2d9b785bc1186ce9aab
Stefan Nairz <stefan.nairz@gmail.com>
2022-06-13 14:55:06 +0200

    added Tests for the Task class, finished the explanation of my solution.

---
+++ coolOrange_CandidateChallenge_Tests/TaskTests.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+using coolOrange_CandidateChallenge_Solution;
+
+namespace coolOrange_CandidateChallenge_Tests
+{
+    [TestFixture]
+    public class TaskTests
+    {
+        [Test]
+        public void CompareToTest()
+        {
+            Task doingHomework = new Task("Doing Homework");
+            doingHomework.SetPriority(10);
+            doingHomework.SetComplexity(8);
+
+            Task eatingLunch = new Task("Eating Lunch");
+            eatingLunch.SetPriority(1);
+            eatingLunch.SetComplexity(2);
+
+            Task programming = new Task("Programming");
+            programming.SetPriority(5);
+            programming.SetComplexity(5);
+
+            Assert.AreEqual(doingHomework, eatingLunch.CompareTo(doingHomework));
+            Assert.AreEqual(programming, eatingLunch.CompareTo(programming));
+            Assert.AreEqual(doingHomework, doingHomework.CompareTo(eatingLunch));
+            Assert.AreEqual(doingHomework, doingHomework.CompareTo(programming));
+            Assert.AreEqual(doingHomework, programming.CompareTo(doingHomework));
+            Assert.AreEqual(programming, programming.CompareTo(eatingLunch));
+        }
+    }
+}
